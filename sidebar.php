46c59365cdff86bebf480e68662713bab4907cc3
Christoph Lupprich <christoph@luppri.ch>
2013-01-02 09:40:40 +0100

    Add styling for sub-categories

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,3 @@
-	
 	<nav id="nav" class="group">
 	<a href="#" id="nav-toggle">hide</a>
 		<?php
@@ -11,10 +10,10 @@
 // http://codex.wordpress.org/Function_Reference/get_categories
  
  foreach( get_categories(array('hide_empty' => '1', 'order_by' => 'name', 'exclude' => array(1))) as $cat ) :
- if( !$cat->parent ) {
- echo '<h2><a href="#">' . $cat->name . '</a></h2><ul>';
- process_cat_tree( $cat->term_id );
- }
+   if( !$cat->parent ) {
+     echo '<h2><a href="#">' . $cat->name . '</a></h2><ul>';
+     process_cat_tree( $cat->term_id );
+   }
  endforeach;
  
  wp_reset_query(); //to reset all trouble done to the original query
@@ -25,24 +24,23 @@ function process_cat_tree( $cat ) {
  $cat_posts = get_posts( $args );
  
  if( $cat_posts ) :
- foreach( $cat_posts as $post ) :
- echo '<li>';
- echo '<a href="' . get_permalink( $post->ID ) . '">' . $post->post_title . '</a>';
- echo '</li>';
- endforeach;
+   foreach( $cat_posts as $post ) :
+     echo '<li>';
+     echo '<a href="' . get_permalink( $post->ID ) . '">' . $post->post_title . '</a>';
+     echo '</li>';
+   endforeach;
  endif;
  
  $next = get_categories('hide_empty=0&parent=' . $cat);
  
  if( $next ) :
- foreach( $next as $cat ) :
- echo '<ul><li><strong>' . $cat->name . '</strong></li>';
- process_cat_tree( $cat->term_id );
- endforeach;
+   foreach( $next as $cat ) :
+     echo '<ul><li><strong>' . $cat->name . '</strong></li>';
+     process_cat_tree( $cat->term_id );
+   endforeach;
  endif;
  
  echo '</ul>';
 }
 ?>
 	</nav>
-
